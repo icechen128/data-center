47089599ce60456dd02b559e31930d5f5de1cfa7
sunbo <sunbo@sunmi.com>
2020-08-24 17:15:00 +0800

    init

---
+++ helper.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func GetData(tableName string) error {
+	db, err := sql.Open("mysql", "root:3541213@tcp(127.0.0.1:3306)/mysql")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer db.Close()
+
+	r, err := db.Query("select * from db")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	fmt.Println(r)
+	r.Err()
+	return err
+}
---
+++ rss_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestParseRss(t *testing.T) {
+	type args struct {
+		url string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "test",
+			args:    args{url: "https://rsshub.app/pcr/news"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := ParseRss(tt.args.url); (err != nil) != tt.wantErr {
+				t.Errorf("ParseRss() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
